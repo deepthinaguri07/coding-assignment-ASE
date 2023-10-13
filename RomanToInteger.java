a6f6eec5971af51cda0c887e4a2fd9ee4b09d948
deepthinaguri07 <147795195+deepthinaguri07@users.noreply.github.com>
2023-10-13 12:13:33 +0530

    Add files via upload

---
+++ RomanToInteger.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine();
+        int result = romanToInt(romanNumeral);
+        System.out.println("The integer value of " + romanNumeral + " is: " + result);
+    }
+
+    public static int romanToInt(String s) {
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int curValue = getValue(c);
+
+            if (curValue < prevValue) {
+                result -= curValue;
+            } else {
+                result += curValue;
+            }
+
+            prevValue = curValue;
+        }
+
+        return result;
+    }
+
+    public static int getValue(char c) {
+        if (c == 'I') return 1;
+        if (c == 'V') return 5;
+        if (c == 'X') return 10;
+        if (c == 'L') return 50;
+        if (c == 'C') return 100;
+        if (c == 'D') return 500;
+        if (c == 'M') return 1000;
+        return 0; 
+    }
+}
